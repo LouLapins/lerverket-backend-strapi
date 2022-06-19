0b0479a52a7bf153fa45f7d9152851250621f252
LouLapins <loulapins@hotmail.com>
2022-06-19 16:55:17 +0200

    Change config file

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'lerverket'),
-            user: env('DATABASE_USERNAME', 'lerverket'),
-            password: env('DATABASE_PASSWORD', '0000'),
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
             ssl: false
         },
         debug: false,
